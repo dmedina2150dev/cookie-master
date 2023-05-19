a311be9aad3c5e06868cccf7cce90e74ec939749
Dajan Medina <dmedina2150dev@gmail.com>
2023-05-19 18:49:38 -0400

    agregamos material UI creamos los temas a utilizar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
+import type { AppProps } from 'next/app';
+
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { darkTheme, lightTheme, customTheme } from '@/themes';
+
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
