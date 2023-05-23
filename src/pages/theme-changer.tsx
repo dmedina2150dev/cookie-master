821c45718062b74cde2dab049a98076bbe23e080
Dajan Medina <dmedina2150dev@gmail.com>
2023-05-22 20:12:24 -0400

    Como se pasan las cookies directo al be

--- src/pages/theme-changer.tsx
+++ src/pages/theme-changer.tsx
@@ -1,4 +1,5 @@
-import { ChangeEvent, useEffect, useState } from 'react';
+import { ChangeEvent, FC, useEffect, useState } from 'react';
+import { GetServerSideProps } from 'next';
 
 import { Card, CardContent, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from '@mui/material';
 
@@ -7,7 +8,9 @@ import Cookies from 'js-cookie';
 import { Layout } from '@/components/layouts';
 
 
-const ThemeChangerPage = () => {
+const ThemeChangerPage: FC = ( props ) => {
+
+    console.log({ props })
 
     const [currentTheme, setCurrentTheme] = useState('light');
 
@@ -48,6 +51,26 @@ const ThemeChangerPage = () => {
             </Card>
         </Layout>
     )
+}
+
+// You should use getServerSideProps when:
+// - Only if you need to pre-render a page whose data must be fetched at request time
+
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+    
+    // console.log({ req })
+
+
+    const { theme = 'light', name = 'No Name' }  = req.cookies;
+
+
+    return {
+        props: {
+            theme,
+            name   
+        }
+    }
 }
 
 export default ThemeChangerPage;
